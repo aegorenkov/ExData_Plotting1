1c4ee78a6112444138728ae74e91d7f5a9c78f40
Alexander Egorenkov <aegorenk@gmail.com>
2015-05-10 12:07:12 -0400

    Record basic requirements

---
+++ plot2.R
@@ -0,0 +1,25 @@
+# Loading the data
+
+## We will only be using data from the dates 2007-02-01 and 2007-02-02.
+
+## You may find it useful to convert the Date and Time variables to
+## Date/Time classes in R using the `strptime()` and `as.Date()`
+## functions.
+
+## Note that in this dataset missing values are coded as `?`.
+
+
+# Making the plot
+
+## Construct the plot and save it to a PNG file with a width of 480
+## pixels and a height of 480 pixels.
+
+### Plot description
+## lineplot
+## Title ""
+## y-axis label: ""Global Active Power (kilowatts)", range: 0-6
+## x-axis label: "", range: ("Thu", "Fri", "Sat")
+
+## Name each of the plot files as `plot1.png`, `plot2.png`, etc.
+
+## Add the PNG file and R code file to your git repository
---
+++ plot4.R
@@ -0,0 +1,22 @@
+# Loading the data
+
+## We will only be using data from the dates 2007-02-01 and 2007-02-02.
+
+## You may find it useful to convert the Date and Time variables to
+## Date/Time classes in R using the `strptime()` and `as.Date()`
+## functions.
+
+## Note that in this dataset missing values are coded as `?`.
+
+
+# Making the plot
+
+## Construct the plot and save it to a PNG file with a width of 480
+## pixels and a height of 480 pixels.
+
+### Plot description
+## 4 plots on one buffer
+
+## Name each of the plot files as `plot1.png`, `plot2.png`, etc.
+
+## Add the PNG file and R code file to your git repository
