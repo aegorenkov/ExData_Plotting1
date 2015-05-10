16f33ce41df01c58138a36ce544f73045d185e03
Alexander Egorenkov <aegorenk@gmail.com>
2015-05-10 17:21:04 -0400

    Add plot 2.

--- plot2.R
+++ plot2.R
@@ -10,13 +10,21 @@ data <- read.table("exdata-data-household_power_consumption/household_power_cons
 ## Date/Time classes in R using the `strptime()` and `as.Date()`
 ## functions.
 
+library(lubridate)
 ## We will only be using data from the dates 2007-02-01 and 2007-02-02.
+data$Date_time = dmy_hms(paste(data$Date, data$Time))
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+#data$Time <- as.Date(data$Time, format = "%H:%M:%S")
 upper_date <- as.Date("2007-02-02", format = "%Y-%m-%d")
 lower_date <- as.Date("2007-02-01", format = "%Y-%m-%d")
 data <- data[data$Date >= lower_date & data$Date <= upper_date,]
 
 # Making the plot
+plot(data$Date_time, data$Global_active_power, 
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)",
+     type = 'n')
+lines(data$Date_time, data$Global_active_power)
 
 ## Construct the plot and save it to a PNG file with a width of 480
 ## pixels and a height of 480 pixels.
@@ -24,7 +32,7 @@ data <- data[data$Date >= lower_date & data$Date <= upper_date,]
 ### Plot description
 ## lineplot
 ## Title ""
-## y-axis label: ""Global Active Power (kilowatts)", range: 0-6
+## y-axis label: "Global Active Power (kilowatts)", range: 0-6
 ## x-axis label: "", range: ("Thu", "Fri", "Sat")
 
 ## Name each of the plot files as `plot1.png`, `plot2.png`, etc.
