0b36c6a3fa35f26e25ca0fd4db2aa8dcd491112d
Alexander Egorenkov <aegorenk@gmail.com>
2015-05-10 13:29:00 -0400

    Add code to load data.

--- plot1.R
+++ plot1.R
@@ -1,13 +1,20 @@
 # Loading the data
-
-## We will only be using data from the dates 2007-02-01 and 2007-02-02.
+## Note that in this dataset missing values are coded as `?`.
+data <- read.table("exdata-data-household_power_consumption/household_power_consumption.txt", 
+                   sep = ';', 
+                   header = TRUE,
+                   na.string = '?',
+                   )
 
 ## You may find it useful to convert the Date and Time variables to
 ## Date/Time classes in R using the `strptime()` and `as.Date()`
 ## functions.
 
-## Note that in this dataset missing values are coded as `?`.
-
+## We will only be using data from the dates 2007-02-01 and 2007-02-02.
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+upper_date <- as.Date("2007-02-02", format = "%Y-%m-%d")
+lower_date <- as.Date("2007-02-01", format = "%Y-%m-%d")
+data <- data[data$Date >= lower_date & data$Date <= upper_date,]
 
 # Making the plot
 
